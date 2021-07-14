c144a1835ae647aabb7b73c95ef7e408313d4cb0
TareqJudehGithub <tareq.judeh@outlook.com>
2021-07-14 02:14:38 +0200

    Uploaded completed chapters and exercises from Lesson: SQL Deep Dive.

--- SQL_Deep_Dive/NULL.sql
+++ SQL_Deep_Dive/NULL.sql
@@ -1,3 +1,14 @@
 NULL
 
--- A NULL value is different from a zero value.
+/* - A NULL value is different from a zero value
+   - NULL values are values that are empty
+   - NULL is used to represent an empty value
+   - Always check for NULLs when necessary
+   - Filter out NULLs
+   - Use IS for filtering NULL, NOT NULL, TRUE, or FALSE
+    */  
+          WHERE name IS NULL;
+          WHERE is_logged IS NOT FALSE;
+          WHERE total IS NOT NULL;
+      /*  
+*/
