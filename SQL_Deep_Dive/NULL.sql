6941cdc01e9eb98874dbfc9d3a5f1817e488b1a1
TareqJudehGithub <tareq.judeh@outlook.com>
2021-07-16 02:16:36 +0200

    Uploaded finished chapters and exercises from Lesson SQL Deep Dive.

--- SQL_Deep_Dive/NULL.sql
+++ SQL_Deep_Dive/NULL.sql
@@ -2,7 +2,7 @@ NULL
 
 /* - A NULL value is different from a zero value
    - NULL values are values that are empty
-   - NULL is used to represent an empty value
+   - NULL is used to represent an empty value, not =
    - Always check for NULLs when necessary
    - Filter out NULLs
    - Use IS for filtering NULL, NOT NULL, TRUE, or FALSE
@@ -12,3 +12,9 @@ NULL
           WHERE total IS NOT NULL;
       /*  
 */
+
+-- This statement below will return everything and reject nothing
+SELECT	bcolor
+FROM distinct_demo
+WHERE bcolor IS NULL
+OR  bcolor IS NOT NULL;
